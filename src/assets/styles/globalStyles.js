cc3609897a43044c67d27fad9c95709edb7c7e57
Jakub Chmielewski <jakubchmielewski80@gmail.com>
2020-03-22 15:07:31 +0100

    basic structure

---
+++ src/assets/styles/globalStyles.js
@@ -0,0 +1,26 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 80px 65px 0;
+    font-family: 'Montserrat';
+  }
+
+  button {
+    padding: 0;
+    cursor: pointer;
+    font-family: 'Montserrat';
+  }
+  
+  p {
+    font-size: 16px;
+  }
+
+  ul {
+    padding: 0;
+    margin: 0;
+  }
+`;
+
+export default GlobalStyle;
